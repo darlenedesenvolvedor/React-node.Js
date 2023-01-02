95feb95974f1b7428c2a6aed0c2a7313e3f54d9b
Darlene Moura <mdarlenemoura@gmail.com>
2023-01-02 14:50:40 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import{Pesquisa} from './Containers/Pesquisa'
+
+function App(){
+    return Pesquisa()
+}
+
+export default App;
---
+++ src/Containers/Pesquisa.js
@@ -0,0 +1,11 @@
+export function Pesquisa() {
+    return (
+     <div className="App">
+       <header className="App-header">
+         <p> Qual CEP você deseja pesquisar?</p>
+         <input/>
+         <button>CONSULTAR</button>
+       </header>
+     </div>
+   )
+ }
